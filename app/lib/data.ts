bb7eb92bf61e6b33b42b98744a9e75588e16c1b2
yohan <yohanneediston@gmail.com>
2025-11-27 14:54:02 +0800

    UI

--- app/lib/data.ts
+++ app/lib/data.ts
@@ -61,7 +61,7 @@ export async function fetchCardData() {
          SUM(CASE WHEN status = 'paid' THEN amount ELSE 0 END) AS "paid",
          SUM(CASE WHEN status = 'pending' THEN amount ELSE 0 END) AS "pending"
          FROM invoices`;
-
+         
     const data = await Promise.all([
       invoiceCountPromise,
       customerCountPromise,
